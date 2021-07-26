c919969cff9971c5f8918a7332cd3c07d1e54fb4
cauasimiao <caua.simiao@outlook.com>
2021-07-26 13:35:15 -0300

    Adicionar arquivos de projeto.

---
+++ LivrariaAPI/Models/Job.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+
+#nullable disable
+
+namespace LivrariaAPI.Models
+{
+    public partial class Job
+    {
+        public Job()
+        {
+            Users = new HashSet<User>();
+        }
+
+        public short JobId { get; set; }
+        public string JobDesc { get; set; }
+
+        public virtual ICollection<User> Users { get; set; }
+    }
+}
